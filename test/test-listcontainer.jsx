cf4be9c11a88cd4314c4671b247a95971070ab14
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-22 17:20:15 -0400

    Start listcontainer test

---
+++ test/test-listcontainer.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
+var should = require('chai').should();
+
+var Listcontainer = require('../js/listcontainer');
+var renderer = TestUtils.createRenderer();
+
+describe('Listcontainer component', function() {
+  it('Renders HTML', function() {
+    renderer.render(<Listcontainer title="listContainer"/>);
+    var result = renderer.getRenderOutput();
+    console.log(result, "<<< result");
+    result.props.title.should.equal()
+  })
+
+})
