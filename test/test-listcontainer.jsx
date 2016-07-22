2b1581dae092dcefbbf200d30121f6a27c5ec7af
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-22 17:33:43 -0400

    Compare listcontainer type to list component

--- test/test-listcontainer.jsx
+++ test/test-listcontainer.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var TestUtils = require('react-addons-test-utils');
 var should = require('chai').should();
+var List = require('../js/list');
 
 var Listcontainer = require('../js/listcontainer');
 var renderer = TestUtils.createRenderer();
@@ -10,7 +11,6 @@ describe('Listcontainer component', function() {
     renderer.render(<Listcontainer title="listContainer"/>);
     var result = renderer.getRenderOutput();
     console.log(result, "<<< result");
-    result.props.title.should.equal()
-  })
-
-})
+    result.props.title.should.equal();
+  });
+});
